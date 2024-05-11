2b5fb13021fdd4527d0c2b3ddadc61f0d98bd7d4
Murad Lalayev <lalayevmurad@gmail.com>
2024-05-11 18:06:28 +0400

    my commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import './header.css'
-import { IoPersonOutline } from "react-icons/io5";
+import React from 'react';
+import { useState } from 'react';
+import './header.css';
+import main from '../../assets/main.png';
+import logo from '../../assets/logo.svg';
 import { CgShoppingCart } from "react-icons/cg";
+import { IoPersonOutline } from "react-icons/io5";
+import { IoMdArrowDropdown } from "react-icons/io";
 import { MdOutlineEuroSymbol } from "react-icons/md";
-import logo from '../../assets/logo.svg'
-import main from '../../assets/main.png'
 
 function header() {
+
+    const [isRotated, setIsRotated] = useState(false);
+
+    const handleClick = () => {
+      setIsRotated(!isRotated);
+    };
+
+
     return (
 
         <div className="upperpart">
 
             <header>
-                <img src={logo} className='logo' />
+                <a href="#"><img src={logo} className='logo' /></a>
 
                 <ul className='headerul'>
                     <li><a href="#">Multi-day tours</a></li>
@@ -22,9 +32,11 @@ function header() {
                 </ul>
                 <div className="btnsdv">
                     <div className="lftbtnsdv">
+
                         <button><IoPersonOutline size={20} className='icon' /></button>
                         <button><CgShoppingCart size={20} className='icon' /></button>
-                        <button><MdOutlineEuroSymbol size={20} className='icon' /></button>
+                        <button onClick={handleClick}><MdOutlineEuroSymbol size={20} className='icon' /> <IoMdArrowDropdown size={16} style={{ transform: isRotated ? 'rotate(180deg)' : 'none' }}/></button>
+
                     </div>
 
                     <div className="rghtbtnsdv">
