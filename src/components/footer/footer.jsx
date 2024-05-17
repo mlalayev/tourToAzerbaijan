75b639e11cf0e5aabea40b31450bc11edb32d3af
Murad Lalayev <lalayevmurad@gmail.com>
2024-05-17 13:51:12 +0400

    my commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,46 +2,48 @@ import React from 'react'
 import './footer.css'
 
 function footer() {
-  return (
-    <footer>
-        <div className="containerftr">
-            <div className="travelservices">
-                <p>Travel sevices</p>
-                <div className="horizontall"></div>
-                <a href="#">Multi-day tours</a>
-                <a href="#">Multi-day tours</a>
-                <a href="#">Multi-day tours</a>
-                <a href="#">Multi-day tours</a>
-            </div>
+    return (
+        <footer>
+            <div className="containerftr">
+                <div className="travelservices">
+                    <p>Travel sevices</p>
+                    <div className="horizontall"></div>
+                    <a href="#">Multi-day tours</a>
+                    <a href="#">Multi-day tours</a>
+                    <a href="#">Multi-day tours</a>
+                    <a href="#">Multi-day tours</a>
+                </div>
 
-            <div className="companies">
-                <p>Companies</p>
-            <div className="horizontall"></div>
-                <a href="#">About Azerbaijan tours</a>
-                <a href="#">About Azerbaijan tours</a>
-                <a href="#">About Azerbaijan tours</a>
-            </div>
+                <div className="companies">
+                    <p>Companies</p>
+                    <div className="horizontall"></div>
+                    <a href="#">About Azerbaijan tours</a>
+                    <a href="#">About Azerbaijan tours</a>
+                    <a href="#">About Azerbaijan tours</a>
+                </div>
 
-            <div className="workwithus">
-                <p>Work with us</p>
-                <div className="horizontall"></div>
-                <a href="#">for travel agents</a>
-            </div>
+                <div className="workwithus">
+                    <p>Work with us</p>
+                    <div className="horizontall"></div>
+                    <a href="#">for travel agents</a>
+                </div>
 
-            <div className="officeinbaku">
-                <p>Office in Baku</p>
-                <div className="horizontall"></div>
-                <p>+994 50 274 01 81</p>
+                <div className="contact">
+                    <div className="officeinbaku">
+                        <p>Office in Baku</p>
+                        <div className="horizontall"></div>
+                        <p>+994 50 274 01 81</p>
+                    </div>
+                    <br />
+                    <div className="officeinbaku">
+                        <p>Office in Baku</p>
+                        <div className="horizontall"></div>
+                        <p>+994 50 274 01 81</p>
+                    </div>
+                </div>
             </div>
-
-            <div className="officeinbaku">
-                <p>Office in Baku</p>
-                <div className="horizontall"></div>
-                <p>+994 50 274 01 81</p>
-            </div>
-        </div>
-    </footer>
-  )
+        </footer>
+    )
 }
 
 export default footer
