3062af913fd67adabe214a4ba9380c9e30c54331
Murad Lalayev <lalayevmurad@gmail.com>
2024-05-23 14:53:11 +0400

    my commit

--- src/components/sliders/slider.jsx
+++ src/components/sliders/slider.jsx
@@ -9,7 +9,16 @@ function slider(props) {
             <div className="txtprtscndsc">
                 <h1 className='propshuno'>{props.huno}</h1>
                 <p className='propsp'>{props.p}</p>
-                <button className='propsbtn'>{props.btn} <FaArrowRightLong /> </button>
+                {/* <button className='propsbtn'>{props.btn} <FaArrowRightLong /> </button> */}
+            
+                <button className="cta">
+                                    <span>Hover me</span>
+                                    <svg width="15px" height="10px" viewBox="0 0 13 10">
+                                        <path d="M1,5 L11,5"></path>
+                                        <polyline points="8 1 12 5 8 9"></polyline>
+                                    </svg>
+                                </button>
+
             </div>
         </div>
     )
