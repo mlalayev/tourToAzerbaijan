dd6b035e64cc0d25964dfdcc40ed5b044df8d480
Murad Lalayev <lalayevmurad@gmail.com>
2024-05-25 14:01:15 +0400

    my commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,45 +12,60 @@ import { RxHamburgerMenu } from "react-icons/rx";
 function header() {
 
     const [isRotated, setIsRotated] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleClick = () => {
         setIsRotated(!isRotated);
     };
 
+    const handleHamburgerClick = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <header>
             <div className="container">
-                <a href="/home"><img src={logo} className='logo' /></a>
+                <a href="/home"><img src={logo} className="logo" alt="Logo" /></a>
 
-                <ul className='headerul'>
+                <ul className="headerul">
                     <li><a href="#">Multi-day tours</a></li>
                     <li><a href="/sngldytrips">Day trips</a></li>
                     <li><a href="#">Visa-free shore tours</a></li>
                     <li><a href="#">Visa informations</a></li>
                 </ul>
-                <div className="lftbtnsdv">
 
+                <div className="lftbtnsdv">
                     <button>
-                        <GoPerson size={20} className='icon' id='iconfirst' strokeWidth={1} />
+                        <GoPerson size={20} className="icon" id="iconfirst" strokeWidth={1} />
                     </button>
                     <button>
-                        <CgShoppingCart size={20} className='icon' />
+                        <CgShoppingCart size={20} className="icon" />
                     </button>
                     <button onClick={handleClick}>
-                        <MdOutlineEuroSymbol size={20} className='icon' />
+                        <MdOutlineEuroSymbol size={20} className="icon" />
                         <RiArrowDownSLine strokeWidth={1} size={16} style={{ transform: isRotated ? 'rotate(180deg)' : 'none' }} />
                     </button>
-
                 </div>
 
                 <div className="rghtbtnsdv">
-                    <button className='chcktbtn'>Checkout</button>
+                    <button className="chcktbtn">Checkout</button>
                 </div>
 
-                <div className="hamdiv">
-                    <RxHamburgerMenu className='hamburger' size={20} color='black' />
+                <div className="hamdiv" onClick={handleHamburgerClick}>
+                    <RxHamburgerMenu className="hamburger" size={20} color="black" />
                 </div>
             </div>
+
+            {isMenuOpen && (
+                <div className="hamburger-menu">
+                    <ul>
+                        <li><a href="#">Multi-day tours</a></li>
+                        <li><a href="/sngldytrips">Day trips</a></li>
+                        <li><a href="#">Visa-free shore tours</a></li>
+                        <li><a href="#">Visa informations</a></li>
+                    </ul>
+                </div>
+            )}
         </header>
     )
 }
