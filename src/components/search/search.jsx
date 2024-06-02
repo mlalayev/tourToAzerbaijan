95f8e126e2730dc6e46199f5e89c4b60b579b1e9
Murad Lalayev <lalayevmurad@gmail.com>
2024-06-02 17:04:41 +0400

    my commit

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
 import './search.css'
+import React, { useState } from 'react';
 import airports from '../../../airports.json'; // Ensure this path is correct
-
+import skyscanner from '../../assets/poweredbyskyscanner.png'
 const SearchForm = () => {
     const [from, setFrom] = useState('');
-    const [to, setTo] = useState('');
+    const [hotel, setHotel] = useState('');
+    const [to, setTo] = useState('Heydar Aliyev International Airport - GYD - Baku, Azerbaijan');
     const [fromSuggestions, setFromSuggestions] = useState([]);
+    const [hotelSuggestions, setHotelSuggestions] = useState([]);
     const [toSuggestions, setToSuggestions] = useState([]);
     const [adults, setAdults] = useState(1);
     const [children, setChildren] = useState(0);
@@ -27,6 +29,22 @@ const SearchForm = () => {
         }
     };
 
+    const handleHotelChange = (e) => {
+        const value = e.target.value;
+        setHotel(value);
+        if (value.length > 1) {
+            setHotelSuggestions(
+                airports.filter((airport) =>
+                    airport.name.toLowerCase().includes(value.toLowerCase()) ||
+                    airport.code.toLowerCase().includes(value.toLowerCase()) ||
+                    airport.city.toLowerCase().includes(value.toLowerCase())
+                )
+            );
+        } else {
+            setHotelSuggestions([]);
+        }
+    };
+
     const handleToChange = (e) => {
         const value = e.target.value;
         setTo(value);
@@ -44,12 +62,17 @@ const SearchForm = () => {
     };
 
     const selectFromSuggestion = (suggestion) => {
-        setFrom(`${suggestion.name} - ${suggestion.code} - ${suggestion.city}, ${suggestion.country}`);
+        setFrom(`${suggestion.name} - ${suggestion.code} - ${suggestion.city} - ${suggestion.country}`);
         setFromSuggestions([]);
     };
 
+    const selectHotelSuggestion = (suggestion) => {
+        setFrom(`${suggestion.name} - ${suggestion.code} - ${suggestion.city} - ${suggestion.country}`);
+        setHotelSuggestions([]);
+    };
+
     const selectToSuggestion = (suggestion) => {
-        setTo(`${suggestion.name} - ${suggestion.code} - ${suggestion.city}, ${suggestion.country}`);
+        setTo(`${suggestion.name} - ${suggestion.code} - ${suggestion.city} - ${suggestion.country}`);
         setToSuggestions([]);
     };
 
@@ -77,6 +100,9 @@ const SearchForm = () => {
 
     return (
         <div className='search-container'>
+
+            <img src={skyscanner} className='skyscanner' />
+
             <div className="tab-container">
                 <button className={`tab ${activeTab === 'flights' ? 'active' : ''}`} onClick={() => handleTabChange('flights')}>Flights</button>
                 <button className={`tab ${activeTab === 'hotels' ? 'active' : ''}`} onClick={() => handleTabChange('hotels')}>Hotels</button>
@@ -87,15 +113,15 @@ const SearchForm = () => {
                     <div className="form-group-one">
                         <div>
                             <input type="radio" id="option1" name="options" value="option1" />
-                            <label htmlFor="option1">Option 1</label>
+                            <label htmlFor="option1">One-way</label>
                         </div>
                         <div>
                             <input type="radio" id="option2" name="options" value="option2" />
-                            <label htmlFor="option2">Option 2</label>
+                            <label htmlFor="option2">Two-way</label>
                         </div>
                         <div>
                             <input type="radio" id="option3" name="options" value="option3" />
-                            <label htmlFor="option3">Option 3</label>
+                            <label htmlFor="option3">Multi-city</label>
                         </div>
                     </div>
 
@@ -113,14 +139,12 @@ const SearchForm = () => {
                                 {fromSuggestions.length > 0 && (
                                     <ul className="suggestions">
                                         {fromSuggestions.map((suggestion, index) => (
-                                            <li key={index} onClick={() => selectToSuggestion(suggestion)}>
+                                            <li key={index} onClick={() => selectFromSuggestion(suggestion)}>
                                                 {suggestion.name} - {suggestion.code} - {suggestion.city}, {suggestion.country}
                                             </li>
                                         ))}
                                     </ul>
                                 )}
-                            </div>
-                            <div className="autocomplete">
                                 <input
                                     placeholder="To"
                                     type="text"
@@ -139,11 +163,90 @@ const SearchForm = () => {
                                     </ul>
                                 )}
                             </div>
-                            <input placeholder="Type Here" type="date" className="textInputthree" />
-                            <input placeholder="Type Here" type="date" className="textInputfour" />
+
+                            <div className="flexend">
+                                <input placeholder="Type Here" type="date" className="textInputthree" />
+                                <input placeholder="Type Here" type="date" className="textInputfour" />
+                            </div>
+
                         </div>
 
                         <div className="form-group-three">
+
+                            <div className="counter">
+                                <label>Adults: </label>
+                                <div className='flex'>
+                                    <button className='indecbtn' type="button" onClick={() => handleAdultsChange(-1)}>-</button>
+                                    <h1>{adults}</h1>
+                                    <button className='indecbtn' type="button" onClick={() => handleAdultsChange(1)}>+</button>
+                                </div>
+                            </div>
+
+                            <div className="counter">
+                                <label>Children: </label>
+                                <div className='flex'>
+                                    <button className='indecbtn' type="button" onClick={() => handleChildrenChange(-1)}>-</button>
+                                    <h1>{children}</h1>
+                                    <button className='indecbtn' type="button" onClick={() => handleChildrenChange(1)}>+</button>
+                                </div>
+                            </div>
+
+                            <div className="flex">
+                                <label>Cabin class</label>
+                                <select className="idun">
+                                    <option>Economy</option>
+                                    <option>Business</option>
+                                    <option>First</option>
+                                </select>
+                            </div>
+
+                        </div>
+
+                        <button className='procedbtn' type="button" onClick={() => window.open("https://www.skyscanner.com/", "_blank")}>Search flights</button>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className="search-hotels-container">
+                        <div className="form-group-hotels">
+                            <input
+                                placeholder="Hotel names..."
+                                type="text"
+                                className="textInputone scndinput"
+                                onChange={handleHotelChange}
+                                onKeyDown={handleEnterPress}
+                            />
+
+                            {toSuggestions.length > 0 && (
+                                <ul className="suggestions">
+                                    {toSuggestions.map((suggestion, index) => (
+                                        <li key={index} onClick={() => selectHotelSuggestion(suggestion)}>
+                                            {suggestion.name} - {suggestion.code} - {suggestion.city}, {suggestion.country}
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+
+                            <div className="flexx">
+                                <input
+                                    placeholder="From"
+                                    type="date"
+                                    className="textInputone scndinputdate"
+                                    // value={from}
+                                    // onChange={handleFromChange}
+                                    onKeyDown={handleEnterPress}
+                                />
+                                <input
+                                    placeholder="Till"
+                                    type="date"
+                                    className="textInputone scndinputdate"
+                                    // value={from}
+                                    // onChange={handleFromChange}
+                                    onKeyDown={handleEnterPress}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-group-four">
                             <div className="counter">
                                 <label>Adults: </label>
                                 <div className='flex'>
@@ -160,37 +263,9 @@ const SearchForm = () => {
                                     <button className='indecbtn' type="button" onClick={() => handleChildrenChange(1)}>+</button>
                                 </div>
                             </div>
-                            <div className="flex">
-                                <label>Cabin class</label>
-                                <select className="idun">
-                                    <option>Economy</option>
-                                    <option>Business</option>
-                                    <option>First</option>
-                                </select>
-                            </div>
                         </div>
-
-                        <button className='procedbtn' type="button" onClick={() => window.open("https://www.skyscanner.com/", "_blank")}>Search flights</button>
+                        <button className='procedbtn' type="button" onClick={() => window.open("https://www.skyscanner.com/", "_blank")}>Search hotels</button>
                     </div>
-                </>
-            ) : (
-                <>
-                    <div className="form-group">
-                        <input type="text" placeholder="Destination" />
-                        <input type="date" placeholder="Check-in" />
-                        <input type="date" placeholder="Check-out" />
-                    </div>
-                    <div className="form-group-four">
-                        <div>
-                            <label>Adults</label>
-                            <input type="number" min="1" defaultValue="1" />
-                        </div>
-                        <div>
-                            <label>Children</label>
-                            <input type="number" min="0" defaultValue="0" />
-                        </div>
-                    </div>
-                    <button type="button" onClick={() => window.location.href = '/hotels'}>Search hotels</button>
                 </>
             )}
         </div>
